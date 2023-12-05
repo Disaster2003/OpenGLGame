acc5d7f8f58bb75fbe84c47034270498965d25d0
Junpei <JunpeiYoshino20031017@gmail.com>
2023-12-05 11:07:47 +0900

    テクスチャの表示.cpp

--- OpenGLGame/Res/standard.frag
+++ OpenGLGame/Res/standard.frag
@@ -6,11 +6,20 @@
 // シェーダへの入力
 layout(location=0) in vec4 inColor; // 頂点色
 
+// テクスチャサンプラ
+layout(binding=0) uniform sampler2D texColor;
+
 // 出力する色データ
 // out修飾子 : シェーダからの出力の格納
 out vec4 outColor;
 
 void main()
 {
-  outColor = inColor;
+	vec4 c = 
+		texture
+		(
+			texColor,				// サンプラ変数
+			gl_FragCoord.xy * 0.01	// テクスチャ座標
+		);
+	outColor = c * inColor;
 }
--- OpenGLGame/Src/Main.cpp
+++ OpenGLGame/Src/Main.cpp
@@ -13,10 +13,10 @@
 /// <summary>
 /// OpenGLからのメッセージを処理するコールバック関数
 /// </summary>
-/// <param name="source">メッセージの発信者(OpenGL、Windows、シェーダなど)</param>
-/// <param name="type">メッセージの種類(エラー、警告など)</param>
+/// <param name="source">メッセージの発信者(OpenGL,Windows,シェーダなど)</param>
+/// <param name="type">メッセージの種類(エラー,警告など)</param>
 /// <param name="id">メッセージを一位に識別する値</param>
-/// <param name="severity">メッセージの重要度(高、中、低、最低)</param>
+/// <param name="severity">メッセージの重要度(高,中,低,最低)</param>
 /// <param name="length">メッセージの文字数. 負数ならメッセージは0終端されている</param>
 /// <param name="message">メッセージ本体</param>
 /// <param name="userParam">コールバック設定時に指定したポインタ</param>
@@ -117,6 +117,90 @@ GLuint CompileShader
     // 引数 : シェーダの管理番号
     glCompileShader(object);
     return object;
+}
+
+/// <summary>
+/// テクスチャを読み込む
+/// </summary>
+/// <param name="filename">テクスチャファイル名</param>
+/// <returns>テクスチャの管理番号</returns>
+GLuint LoadTexture(const char* filename)
+{
+  // ファイルを開く
+  // std::ios::binary : バイナリモードの指定
+  std::ifstream file
+  (
+      filename,
+      std::ios::binary
+  );
+  // ファイルが正常に開けない(false)時,
+  // エラーメッセージを出力して,
+  // 0(オブジェクトが存在しない)を返して終了
+  if ( ! file)
+  {
+    char s[256];
+    snprintf
+    (
+        s,
+        256,
+        "[ERROR] %s: %sを開けません\n",
+        __func__,
+        filename
+    );
+    OutputDebugStringA(s);
+    return 0;
+  }
+
+  // ファイルサイズの取得
+// 引数 : ファイルパス
+  const size_t filesize = std::filesystem::file_size(filename);
+  // ファイルサイズ分の領域を用意
+  std::vector<uint8_t> buffer(filesize);
+  // ファイルの読み込み
+  file.read
+  (
+      reinterpret_cast<char*>(buffer.data()),   // バッファアドレス
+      filesize                                  // 読み込むバイト数
+  );
+  // ファイルを閉じる
+  file.close();
+
+  // ヘッダから情報を取得
+  const size_t tgaHeaderSize = 18; // ヘッダ情報のバイト数
+  const int width = buffer[12] + buffer[13] * 256;
+  const int height = buffer[14] + buffer[15] * 256;
+
+  GLuint object = 0; // テクスチャの管理番号
+  // テクスチャの作成
+  glCreateTextures
+  (
+      GL_TEXTURE_2D,    // テクスチャの種類
+      1,                // 作成する個数
+      &object           // 番号を格納する配列のアドレス
+  );
+  // 画像データ分のGPUメモリ領域の確保
+  glTextureStorage2D
+  (
+      object,   // テクスチャの管理番号
+      1,        // 作成するレベル数(ミップマップ)
+      GL_RGBA8, // ピクセル形式
+      width,    // 幅
+      height    // 高さ
+  );
+  // 画像データをGPUメモリにコピー
+  glTextureSubImage2D
+  (
+      object,                       // テクスチャの管理番号
+      0,                            // コピー先のレイヤー番号
+      0,                            // コピー先のX座標(複数の画像をひとつにまとめる用 ex)スプライトシート,テクスチャアトラス)
+      0,                            // コピー先のY座標(複数の画像をひとつにまとめる用 ex)スプライトシート,テクスチャアトラス)
+      width,                        // コピーする画像の幅
+      height,                       // コピーする画像の高さ
+      GL_BGRA,                      // ピクセルに含まれる要素と順序
+      GL_UNSIGNED_BYTE,             // 要素の型
+      buffer.data() + tgaHeaderSize // 画像データのアドレス
+  );
+  return object;
 }
 
 /// <summary>
@@ -296,7 +380,7 @@ int WINAPI WinMain
     // IBOをOpenGLコンテキストとVAOの両方にバインド
     // (VAOがOpenGLコンテキストにバインドされているため)
     // GL_ELEMENT_ARRAY_BUFFER
-    // ->OpenGLコンテキストにVAOが割り当てられている場合、
+    // ->OpenGLコンテキストにVAOが割り当てられている場合,
     //   バッファオブジェクトをVAOにも割り当てる
     glBindBuffer
     (
@@ -304,7 +388,7 @@ int WINAPI WinMain
         ibo                       // 割り当てるインデックスバッファの管理番号
     );
 
-    // VBOをOpenGLコンテキストにバインド(IBOと異なり、VAOにはバインドされない)
+    // VBOをOpenGLコンテキストにバインド(IBOと異なり,VAOにはバインドされない)
     glBindBuffer
     (
         GL_ARRAY_BUFFER,  // 割り当てるオブジェクトの種類
@@ -316,7 +400,7 @@ int WINAPI WinMain
     glEnableVertexAttribArray(0);
 
     // 0番目の頂点属性を設定
-    // このとき、OpenGLコンテキストにバインドされているVBOが、頂点属性にバインドされる
+    // このとき,OpenGLコンテキストにバインドされているVBOが,頂点属性にバインドされる
     glVertexAttribPointer
     (
         0,        // 頂点属性配列のインデックス
@@ -326,6 +410,10 @@ int WINAPI WinMain
         0,        // 次のデータまでのバイト数
         0         // 最初のデータの位置
     );
+#pragma endregion
+
+#pragma region テクスチャの作成
+    GLuint tex = LoadTexture("Res/box.tga");
 #pragma endregion
 
 #pragma region メインループの定義
@@ -364,6 +452,15 @@ int WINAPI WinMain
             prog3D,         // プログラムオブジェクトの管理番号
             0,              // 送り先ロケーション番号
             timer * 0.5f    // 送るデータ
+        );
+
+        // 描画に使うテクスチャを
+        // (テクスチャ・イメージ・ユニットに)割り当て
+        glBindTextures
+        (
+            0,      // 割り当て開始インデックス
+            1,      // 割り当てる個数
+            &tex    // テクスチャ管理番号配列のアドレス
         );
 
         // 図形を描画
